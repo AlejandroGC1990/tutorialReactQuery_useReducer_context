968d43dd37599abaac9e3e9c603c28b5747cf9cb
AlejandroGarcía Carmona <alejandrogarciacarmona@Mac-mini-de-AlejandroGarcia.local>
2023-05-05 17:26:24 +0200

    Defining the counter context in a separate file

--- reduce&context_counter/src/App.js
+++ reduce&context_counter/src/App.js
@@ -1,49 +1,17 @@
-import { useReducer, useContext } from 'react';
-import CounterContext from './CounterContext'
-
-const Display = () => {
-  const [counter, dispatch] = useContext(CounterContext);
-
-  return <div>{counter}</div>
-}
-
-const Button = ({ type, label }) => {
-  const [counter, dispatch] = useContext(CounterContext);
-
-  return (
-    <button onClick={() => dispatch({ type })}>
-      {label}
-    </button>
-  )
-}
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-      return state + 1
-    case "DEC":
-      return state - 1
-    case "ZERO":
-      return 0
-    default:
-      return state
-  }
-}
+import Display from './components/Display';
+import Button from './components/Button';
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
   return (
-    <CounterContext.Provider value={[counter, counterDispatch]}>
-      <Display counter={counter} />
+    <div>
+      <Display />
       <div>
-        <Button dispatch={counterDispatch} type="INC" label='+' />
-        <Button dispatch={counterDispatch} type="DEC" label='-' />
-        <Button dispatch={counterDispatch} type="ZERO" label='0' />
+        <Button type="INC" label='+' />
+        <Button type="DEC" label='-' />
+        <Button type="ZERO" label='0' />
       </div>
-    </CounterContext.Provider>
+    </div>
   )
 }
 
 export default App
-
--- reduce&context_counter/src/index.js
+++ reduce&context_counter/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {CounterContextProvider} from './CounterContext';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <CounterContextProvider>
     <App />
-  </React.StrictMode>
+  </CounterContextProvider>
 );
